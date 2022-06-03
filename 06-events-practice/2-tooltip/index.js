189e1a83ba5ce9b2dadd3e0888d9b10e7a4e450b
ngolkhov <ngolkhov@mts.ru>
2022-06-03 10:58:39 +0300

    реализация sortable-table-v1 реализация 6 модуля заданий

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,74 @@
 class Tooltip {
-  initialize () {
+  element;
+  distanceFromCursor = 10;
 
+  static currentTooltip = null;
+
+  constructor() {
+    if (!Tooltip.currentTooltip) {
+      Tooltip.currentTooltip = this;
+    } else {
+      return Tooltip.currentTooltip;
+    }
+  }
+
+  render() {
+    const tooltip = document.createElement("div");
+    tooltip.classList.add("tooltip");
+    this.element = tooltip;
+    document.body.append(this.element);
+  }
+
+  initialize() {
+    const tooltip = document.createElement("div");
+    tooltip.classList.add("tooltip");
+    this.element = tooltip;
+
+    document.addEventListener("pointerover", this.showTooltip);
+    document.addEventListener("pointerout", this.deleteTooltip);
+  }
+
+  showTooltip = (event) => {
+    if (event.target.dataset.tooltip === undefined) {
+      return;
+    }
+    if (Tooltip.currentTooltip) {
+      Tooltip.currentTooltip.remove();
+    }
+
+    document.addEventListener("mousemove", this.moveTooltip);
+
+    this.element.style.left = event.pageX + this.distanceFromCursor + "px";
+    this.element.style.top = event.pageY + this.distanceFromCursor + "px";
+    this.element.style.display = "block";
+    this.element.innerHTML = event.target.dataset.tooltip;
+
+    Tooltip.currentTooltip = this.element;
+    document.body.append(this.element);
+  };
+
+  moveTooltip = (event) => {
+    if (Tooltip.currentTooltip) {
+      this.element.style.left = event.pageX + this.distanceFromCursor + "px";
+      this.element.style.top = event.pageY + this.distanceFromCursor + "px";
+    }
+  };
+
+  deleteTooltip() {
+    if (Tooltip.currentTooltip) {
+      Tooltip.currentTooltip.remove();
+    }
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
   }
 }
 
